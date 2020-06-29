17b45967f3f301e6361b4ed3f7c7c1c3fa8afc11
rboudrouss <40902229+rboudrouss@users.noreply.github.com>
2020-06-29 17:18:00 +0100

    small changes

--- flask_app.py
+++ flask_app.py
@@ -1,5 +1,6 @@
 # codé par cyber vladimir poutine et Rboud-sensei
 from flask import Flask, render_template, request
+from math import ceil
 
 app = Flask(__name__)
 
@@ -11,12 +12,14 @@ def home():
 
 @app.route('/calcul', methods=['POST', 'GET'])
 def calcul():
+    # POST CASE
     if request.method == "POST":
         # déclaration & assignation
         notes = []
-        FR_ecrit = request.form["FR_ecrit"]
-        FR_oral = request.form["FR_oral"]
-        TPE = request.form["TPE"]
+        FR_ecrit = ceil(float(request.form["FR_ecrit"]))
+        FR_oral = ceil(float(request.form["FR_oral"]))
+        TPE = ceil(float(request.form["TPE"]))
+        TPE = (TPE-10) if TPE>10 else 0
         try:
             oib = request.form["oib"] == "on"
         except:
@@ -24,7 +27,7 @@ def calcul():
         spe = request.form["spe"]
         for i in range(1, 3):
             for j in ["sport", "philo", "phch", "svt", "math", "lv1", "hg", "lv2", "spe", "option"]:
-                notes.append(request.form[j + str(i)])
+                notes.append(ceil(float(request.form[j + str(i)])))
         if not notes[9]:
             option = False
         else:
@@ -47,19 +50,22 @@ def calcul():
         moyenne_m = []
         for i in range(len(notes) // 2 + 1):
             try:
-                moyenne_m.append((float(notes[i]) + float(notes[i + 10])) / 2)
+                moyenne_m.append((notes[i] + notes[i + 10]) / 2)
             except:
                 break
+        
         # calcul
         somme_des_coefs = coefs[0] * 3 + coefs[1] + \
             coefs[2] + coefs[3] + coefs[4] + coefs[5] + coefs[6]
         if option:
             moyenne_tot = (moyenne_m[4] * coefs[4] + moyenne_m[2] * coefs[2] + moyenne_m[3] * coefs[3] + moyenne_m[6] * coefs[6] + moyenne_m[5] * coefs[5] + moyenne_m[7] * coefs[0] +
-                           moyenne_m[1] * coefs[1] + float(FR_oral) * coefs[0] + float(FR_ecrit) * coefs[0] + ((float(TPE) - 10) if float(TPE) > 10 else 0) * coefs[0] + ((moyenne_m[9] - 10) if moyenne_m[9] > 10 else 0) * coefs[0] + moyenne_m[0] * coefs[0]) / somme_des_coefs
+                           moyenne_m[1] * coefs[1] + FR_oral * coefs[0] + FR_ecrit * coefs[0] + TPE * coefs[0] + ((moyenne_m[9] - 10) if moyenne_m[9] > 10 else 0) * coefs[0] + moyenne_m[0] * coefs[0]) / somme_des_coefs
         else:
             moyenne_tot = (moyenne_m[4] * coefs[4] + moyenne_m[2] * coefs[2] + moyenne_m[3] * coefs[3] + moyenne_m[6] * coefs[6] + moyenne_m[5] * coefs[5] + moyenne_m[7] * coefs[0] +
-                           moyenne_m[1] * coefs[1] + float(FR_oral) * coefs[0] + float(FR_ecrit) * coefs[0] + ((float(TPE) - 10) if float(TPE) > 10 else 0) * coefs[0] + moyenne_m[0] * coefs[0]) / somme_des_coefs
+                           moyenne_m[1] * coefs[1] + FR_oral * coefs[0] + FR_ecrit * coefs[0] + TPE * coefs[0] + moyenne_m[0] * coefs[0]) / somme_des_coefs
         return render_template("result.html", res=str(moyenne_tot)[:5])
+    
+    # page visite
     else:
         return render_template("calcul.html")
 
